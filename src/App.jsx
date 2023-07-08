d6a3349de77f83a1a0f45f37ec6430216c29d05a
rodolfodom <rodolfodom7@gmail.com>
2023-07-07 18:11:29 -0600

    add logic elements to fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { styled } from 'styled-components';
 import Welcome from './components/Welcome';
-
+import getData from './logic/getData';
+import CONSTATNTS from './logic/constants';
 
 function App() {
+
   const [data, setData] = useState(null)
+  const [query, setquery] = useState(null)
+  const [lastquery, setLastQuery] = useState(null)
 
   if (data == null) return <Welcome></Welcome>
 
---
+++ src/logic/constants.js
@@ -0,0 +1,7 @@
+import SECRETS from "./secrets"
+
+const CONSTATNTS = {
+    API_ENDPOINT: `https://www.omdbapi.com/?apikey=${SECRETS.API_KEY}&s=`
+}
+
+export default CONSTATNTS   
